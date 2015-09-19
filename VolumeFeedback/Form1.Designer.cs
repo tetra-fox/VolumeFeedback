4a8a6d53285767d87d5d394d1e1553420088f165
Syrexide <wwc.302@gmail.com>
2015-09-18 22:33:54 -0400

    added project

---
+++ VolumeFeedback/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace VolumeFeedback
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "original";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "yosemite";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(60, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(11, 59);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "custom";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(82, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "start minimized";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(82, 46);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "open on startup";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "VolumeFeedback";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            this.contextMenuStrip1.Text = "VolumeFeedback";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 86);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.TopMost = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ VolumeFeedback/Form1.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.Win32;
+using NAudio.CoreAudioApi;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VolumeFeedback.Properties;
+
+namespace VolumeFeedback
+{
+    public partial class Form1 : Form
+    {
+        private static MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+        private MMDevice device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+        private bool volumenew = Settings.Default.newvol;
+        private bool custom = Settings.Default.custom;
+        private static string datapath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"VolumeFeedback\Data");
+        private string customfile = Path.Combine(datapath, "custom.wav");
+
+        private RegistryKey rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+            notifyIcon1.MouseUp += notifyIcon1_MouseUp;
+            device.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+            if (volumenew == true)
+            {
+                radioButton2.Checked = true;
+            }
+            else if (custom == true)
+            {
+                radioButton3.Checked = true;
+            }
+            else
+            {
+                radioButton1.Checked = true;
+            }
+
+            if (Settings.Default.silentstart == true)
+            {
+                checkBox1.Checked = true;
+                DelayedHide();
+            }
+            if (rk.GetValue("VolumeFeedback") == null)
+            {
+                checkBox2.Checked = false;
+            }
+            else
+            {
+                checkBox2.Checked = true;
+            }
+            if (!Directory.Exists(datapath))
+            {
+                Directory.CreateDirectory(datapath);
+            }
+        }
+
+        private async void DelayedHide()
+        {
+            await Task.Delay(1);
+            Hide();
+            notifyIcon1.Visible = true;
+            ShowInTaskbar = false;
+        }
+
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            if (volumenew == true)
+            {
+                SoundPlayer snd = new SoundPlayer(Properties.Resources.volumenew);
+                snd.Play();
+            }
+            else if (radioButton3.Checked == true)
+            {
+                if (customfile == null)
+                {
+                    MessageBox.Show("select a file first!", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    SoundPlayer snd = new SoundPlayer(customfile);
+                    snd.Play();
+                }
+            }
+            else
+            {
+                SoundPlayer snd = new SoundPlayer(Properties.Resources.volume);
+                snd.Play();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings.Default.newvol = false;
+            Settings.Default.Save();
+            volumenew = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings.Default.newvol = true;
+            Settings.Default.Save();
+            volumenew = true;
+        }
+
+        private async void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            volumenew = false;
+            Settings.Default.newvol = false;
+            Settings.Default.custom = true;
+            Settings.Default.Save();
+            switch (radioButton3.Checked)
+            {
+                case true:
+                    button1.Enabled = true;
+                    for (double i = 125; i < 160.01; i += 5)
+                    {
+                        i = Math.Round(i, 2);
+
+                        await Task.Delay(1);
+
+                        var heightint = Convert.ToInt32(i);
+
+                        Height = heightint;
+                    }
+                    return;
+
+                case false:
+                    for (double i = 160; i > 124.99; i -= 5)
+                    {
+                        i = Math.Round(i, 2);
+
+                        await Task.Delay(1);
+
+                        var heightint = Convert.ToInt32(i);
+
+                        Height = heightint;
+                    }
+                    button1.Enabled = false;
+                    return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = ".wav files|*.wav",
+                Title = "select a wav file",
+                Multiselect = false
+            };
+
+            ofd.ShowDialog();
+            if (ofd.FileName == "")
+            {
+                return;
+            }
+            else
+            {
+                File.Copy(ofd.FileName, customfile, true);
+
+                Settings.Default.customfile = customfile;
+                Settings.Default.Save();
+                Settings.Default.Reload();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+            notifyIcon1.Visible = true;
+            ShowInTaskbar = false;
+        }
+
+        private void notifyIcon1_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    Show();
+                    notifyIcon1.Visible = false;
+                    ShowInTaskbar = true;
+                    break;
+
+                case MouseButtons.Right:
+                    contextMenuStrip1.Show(MousePosition);
+                    break;
+            }
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            ShowInTaskbar = true;
+            Show();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (checkBox1.Checked)
+            {
+                case true:
+                    Settings.Default.silentstart = true;
+                    Settings.Default.Save();
+                    break;
+
+                case false:
+                    Settings.Default.silentstart = false;
+                    Settings.Default.Save();
+                    break;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (checkBox2.Checked)
+            {
+                case true:
+                    rk.SetValue("VolumeFeedback", Application.ExecutablePath.ToString());
+                    break;
+
+                case false:
+                    rk.DeleteValue("VolumeFeedback", false);
+                    break;
+            }
+        }
+    }
+}
